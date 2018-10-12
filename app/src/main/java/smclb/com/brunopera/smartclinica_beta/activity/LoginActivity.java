c21fd046376ea242474e2ff1904ece74dd2d98c0
Bruno Michel <brunomichel00@gmail.com>
2018-10-12 12:12:57 -0300

    Initial commit

---
+++ app/src/main/java/smclb/com/brunopera/smartclinica_beta/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package smclb.com.brunopera.smartclinica_beta.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import smclb.com.brunopera.smartclinica_beta.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
